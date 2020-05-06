de0a080004ca94136e367b2becc35a4ff4720e85
RizaHKhan <khanriza@gmail.com>
2020-05-05 21:23:27 -0400

    completed initial articles controller tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -12,9 +12,19 @@ describe ArticlesController do
     create_list :article, 2
     get :index
     json = JSON.parse(response.body)
+    json_data = json['data']
+    expect(json_data.length).to eq(2)
 
-    pp json
-    # json_data = json[:data]
-    # expect(json_data.length).to eq(2)
+    expect(json_data[0]['attributes']).to eq({
+      "title" => "Title 1",
+      "content" => "Content 1",
+      "slug" => "slug-1"
+    })
+
+    expect(json_data[1]['attributes']).to eq({
+      "title" => "Title 2",
+      "content" => "Content 2",
+      "slug" => "slug-2"
+    })
   end
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    sequence(:title) { |n| "My awesome article #{n}" }
-    sequence(:content) { |n| "MyText #{n}" }
-    sequence(:slug) {|n| "my-awesome-slug-#{n}" }
+    sequence(:title) { |n| "Title #{n}" }
+    sequence(:content) { |n| "Content #{n}" }
+    sequence(:slug) {|n| "slug-#{n}" }
   end
 end
